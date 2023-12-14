49f3fbe4b695ac32df8d041274d75382b9adff96
unknown <johannesjordi@rocketmail.com>
2023-12-14 19:00:38 +0700

    Create pratice project

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div>
+      <h3>Hello world</h3>
+    </div>
+  );
+}
